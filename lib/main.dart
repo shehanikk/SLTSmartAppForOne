c7c358eeb95efd66c39ece077943796c309414eb
shehanikk <park.shehani.7@gmail.com>
2024-01-11 10:43:13 +0530

    otp changes

--- lib/main.dart
+++ lib/main.dart
@@ -4,21 +4,29 @@ import 'package:flutter/material.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: FirebaseOptions(
+      apiKey: 'AIzaSyCxJmDcTECvXMk_9-RTKw8ck-zy7douA4o',
+      appId: '1:149260196149:android:b1c4054fa046469f1ad47c',
+      messagingSenderId: '149260196149',
+      projectId: 'esp32-to-firebase-48d58',
+      authDomain: 'esp32-to-firebase-48d58.firebaseapp.com',
+      databaseURL: 'https://esp32-to-firebase-48d58-default-rtdb.asia-southeast1.firebasedatabase.app/', // IMPORTANT!
+      storageBucket: 'esp32-to-firebase-48d58.appspot.com',
+    ),
+  );
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
-
       ),
       debugShowCheckedModeBanner: false,
       home: const NewSplashScreen(),
